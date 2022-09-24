5715dbf3e3b23c7b9bb8226df4298099f1617d0e
TorresCamilo <catorresmolina@unicesar.edu.co>
2022-09-24 09:25:30 -0500

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,53 @@
+<script setup></script>
+
+<template>
+  <main>
+    <header>
+      <h1 class="bg-primary text-white text-center p-2">
+        Cooperativa de Asociados
+      </h1>
+    </header>
+    <section>
+      <article class="content">
+        <div id="imagenprincipal" class="text-center pb-5">
+          <img
+            src="../assets/asociado.jpg"
+            alt="simbolo de educacion"
+            width="20%"
+          />
+        </div>
+        <p>
+          El asociado de la cooperativa podrá realizar el registro ingresando al
+          formulario que esta en la barra lateral.
+          <strong>Nota: No es necesario estar logueado.</strong>
+        </p>
+
+        <p id="intro">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
+          delectus voluptas tempore dignissimos doloremque aut adipisci
+          laudantium eius facere rerum amet, nam, ex dicta, maiores ut totam?
+          Dolorem, nulla quidem? Necessitatibus a quasi ducimus quam non minima
+          fuga earum in vitae aspernatur corporis reiciendis neque quos
+          praesentium nihil voluptate labore, repellat accusamus dolorum ipsum!
+          Ea officiis eveniet libero ducimus similique? Eveniet corporis ea
+          quaerat nemo aut impedit at eum sit. Dicta pariatur dolorem explicabo
+          iste veritatis accusantium inventore earum soluta magnam. Aut nihil
+          labore debitis sunt minima fugit veniam quo? Consectetur, eaque!
+          Distinctio porro rem voluptas dolore, fugiat nam eos veniam
+          consequuntur, debitis iusto cum. Saepe dolorum corporis amet deleniti
+          ex iure, doloremque pariatur, quos architecto culpa praesentium sit
+          obcaecati? Hic quasi ut nobis ipsam quam corrupti et iure mollitia
+          minima. Quae tempora repudiandae cupiditate corporis similique eveniet
+          ipsam modi, dolore dolorum. Perferendis ratione minima rerum atque
+          nostrum numquam repellat!
+        </p>
+        <p class="pespecial">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis
+          ut similique fugiat, neque, sequi molestiae perferendis nesciunt ipsam
+          officia voluptates eligendi, dolor impedit itaque quaerat vero? Natus
+          cum possimus quaerat!
+        </p>
+      </article>
+    </section>
+  </main>
+</template>
